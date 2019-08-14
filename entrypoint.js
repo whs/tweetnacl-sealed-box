7b2fc1e00471905a1632b09418210c8801ee46b7
SilentImp <thesilentimp@gmail.com>
2019-08-14 09:14:15 +0200

    default import added
    1. added default import
    2. leave object import in place for backward compatibility.
    3. please test it before accept it, because I can't `npm i` it on mac.

--- entrypoint.js
+++ entrypoint.js
@@ -3,4 +3,6 @@ export * from './src/consts';
 import seal from './src/seal';
 import open from './src/open';
 
-export {seal, open};
+const sealedbox = {seal, open};
+export default sealedbox;
+export sealedbox;
