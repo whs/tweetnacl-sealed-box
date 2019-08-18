d2b10cdfe3e2b71ce2ee6cb4b2a178947d3d0bf6
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2019-08-18 23:16:17 +0700

    Fixed exporting

--- entrypoint.js
+++ entrypoint.js
@@ -3,6 +3,5 @@ export * from './src/consts';
 import seal from './src/seal';
 import open from './src/open';
 
-const sealedbox = {seal, open};
-export default sealedbox;
-export sealedbox;
+export default {seal, open};
+export {seal, open};
